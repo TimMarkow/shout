086d39e4c3e28e376b5db3e006dcc72a459fc14c
Tim Markowski <timmarkowski@tim-markowskis-macbook-pro.local>
2011-03-03 13:20:55 -0500

    adding Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shout::Application.config.secret_token = '84307e717aa2aaef0e6387524951604774eb8dd99c4701d1e9d58f75a80e877eda286110ed609230e17221dec7f7b3d740fe15bbf513757fbcc1fd44e8d6baa2'
