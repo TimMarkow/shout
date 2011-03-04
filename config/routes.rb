f915bfadb83e42055fc97b31d6b58676701811c8
Tim Markowski <timmarkowski@tim-markowskis-macbook-pro.local>
2011-03-04 15:27:48 -0500

    made default page new

--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,7 @@ Shout::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-    root :to =>  "shouts#index"
+    root :to =>  "shouts#new"
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
